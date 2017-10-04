86b43648380eb7095be3a29acac1b19923139cd2
Anthony Castrio <anthonycastrio@gmail.com>
2017-10-03 20:19:05 -0700

    add reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
+
+import './reset.css';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +11,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Text to Speech</h1>
         </header>
         <div className="App-container">
-          <h2 className="headline">Text to Speech, Just Start Typing!</h2>
-          <input className="main-input" />
+          <div className="text-to-speech">
+            <h2 className="headline">Text to Speech, Just Start Typing!</h2>
+            <input className="main-input" />
+          </div>
         </div>
       </div>
     );
