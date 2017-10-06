1cc549d5756ff1df093a3b1a569fc64fbd1f498a
Anthony Castrio <anthonycastrio@gmail.com>
2017-10-06 07:48:59 -0700

    it speaks!

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,40 @@ import logo from '../img/logo.svg';
 import './reset.css';
 import './App.css';
 
+const synth = window.speechSynthesis;
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentText: null
+      currentText: ''
     };
   }
 
   handleChange = (event) => {
-    console.log(event.target.value);
-    this.setState({ currentText: event.target.value });
+    const value = event.target.value;
+    console.log(value);
+
+    if (value.includes(' ')) {
+      this.handleSubmit(event);
+    } else {
+      this.setState({ currentText: value });
+    }
   };
+
+  handleSubmit = (event) => {
+    event.preventDefault(); // prevent page reload
+    this.textToSpeech(this.state.currentText);
+    this.setState({ currentText: '' });
+  };
+
+  /* https://codepen.io/SteveJRobertson/pen/emGWaR */
+  textToSpeech = (value) => {
+    const utterThis = new SpeechSynthesisUtterance(value);
+    synth.speak(utterThis);
+    console.log("talk about " + value);
+  }
 
   render() {
     return (
@@ -29,9 +50,13 @@ class App extends Component {
         <div className="App-container">
           <div className="text-to-speech">
             <h2 className="headline">Text to Speech, Just Start Typing!</h2>
-            <input className="main-input"
-              placeholder="Start Typing"
-              onChange={ this.handleChange } />
+            <form className="text-form"
+              onSubmit={ this.handleSubmit }>
+              <input className="main-input"
+                placeholder="Start Typing"
+                value={ this.state.currentText }
+                onChange={ this.handleChange } />
+            </form>
           </div>
         </div>
       </div>
